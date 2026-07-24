8edd1645775bbae40b465bea2086159d13619a08
Francis <reisenseo@gmail.com>
2026-07-24 07:03:06 +0300

    first commit

--- resources/views/theme/starlinknew/layouts/app.blade.php
+++ resources/views/theme/starlinknew/layouts/app.blade.php
@@ -113,7 +113,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- Custom overrides -->
-    <link rel="stylesheet" href="{{ asset('assets/starlinknew/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/starlinknew/style.css') }}?v={{ filemtime(public_path('assets/starlinknew/style.css')) }}">
 
 <style>
   :root {
